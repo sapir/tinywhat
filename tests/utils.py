03885ea0cd075b3acbb0ba94810da706a3a2f147
Y. Sapir <yasapir@gmail.com>
2017-10-09 15:39:02 +0300

    Parse test program output and support multiple outputs in assert_eval.

--- tests/utils.py
+++ tests/utils.py
@@ -1,15 +1,24 @@
+import re
 from tempfile import TemporaryFile
 from io import BytesIO
 from subprocess import check_output
 
 
-def what_eval(inp):
+def what_eval(inp, parse=True):
     with TemporaryFile() as tf:
         tf.write(inp)
         tf.seek(0)
+
         output_str = check_output('./test', stdin=tf)
-        return output_str
+
+        if not parse:
+            return output_str
+
+        return re.findall(r'(?m)(?<= -> )[-0-9]+|^(?! -> ).+', output_str)
 
 
-def assert_eval(inp, out):
-    assert what_eval(inp + '\n') == ' -> {}\n'.format(out)
+def assert_eval(inp, *outs):
+    outs = map(str, outs)
+    actual_outs = what_eval(inp + '\n')
+    assert outs == actual_outs, (
+        "expected: {}; actual: {}".format(outs, actual_outs))
