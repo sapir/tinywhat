cf357e46b3d9664325ca69f3b7c0393c89ad44a7
Y. Sapir <yasapir@gmail.com>
2017-10-09 15:48:43 +0300

    Add some function tests.

---
+++ tests/test_func.py
@@ -0,0 +1,46 @@
+from .utils import assert_eval
+
+
+def test_simple_func():
+    assert_eval('(def @a $a 8) (@a)', 1, 8)
+
+
+def test_simple_func_args():
+    assert_eval(
+        '(def @a $a $a)'
+        '(@a 1)'
+        '(@a 2)'
+        '(@a 5)',
+        1,
+        1,
+        2,
+        5)
+
+
+def test_func_args_overwrite_globals():
+    assert_eval(
+        '(def @a $a 3)'
+        '(set $a 10)'
+        '$a'
+        '(@a 8)'
+        '$a',
+        1,
+        10,
+        10,
+        3,
+        8,
+        )
+
+
+def test_func_args_with_offset():
+    assert_eval(
+        '(def @a $d (+ $d $i))'
+        '(def @b $i (+ $i $j))'
+        '(@a 1 2 3)'
+        '(@b 8 9 10)'
+        '$a\n$b\n$c\n$d\n$e\n$i\n$j\n$k\n',
+        1, 1,
+        4,
+        17,
+        0, 0, 0, 1, 2, 8, 9, 10,
+        )
