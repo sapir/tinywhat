f621b97fb7859a51be33080d1aec000effaec280
Y. Sapir <yasapir@gmail.com>
2017-10-09 18:01:39 +0300

    Test remaining math builtins.

--- tests/test_builtins.py
+++ tests/test_builtins.py
@@ -15,3 +15,25 @@ def test_sub():
 def test_mul():
     assert_eval('(* 5 1)', 5)
     assert_eval('(* 10 200)', 2000)
+
+
+def test_div():
+    assert_eval('(/ 1 2)', 0)
+    assert_eval('(/ 2 1)', 2)
+    assert_eval('(/ 8 2)', 4)
+    assert_eval('(/ 9 2)', 4)
+    assert_eval('(/ 10 5)', 2)
+
+
+def test_mod():
+    assert_eval('(% 1 2)', 1)
+    assert_eval('(% 2 1)', 0)
+    assert_eval('(% 7 3)', 1)
+    assert_eval('(% 8 3)', 2)
+    assert_eval('(% 9 3)', 0)
+    assert_eval('(% 10 5)', 0)
+
+
+def test_eq():
+    assert_eval('(= 1 2)', 0)
+    assert_eval('(= 2 2)', 1)
